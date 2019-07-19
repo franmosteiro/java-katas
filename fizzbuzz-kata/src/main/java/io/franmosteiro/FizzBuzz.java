201281100450e74db59338e010cb7518a923e622
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-19 13:12:49 +0200

    2 should be '2'

--- fizzbuzz-kata/src/main/java/io/franmosteiro/FizzBuzz.java
+++ fizzbuzz-kata/src/main/java/io/franmosteiro/FizzBuzz.java
@@ -2,7 +2,11 @@ package io.franmosteiro;
 
 public class FizzBuzz {
 
-    public String print() {
-        return "1";
+    public String print(int number) {
+        if (number == 1) {
+            return "1";
+        } else {
+            return "2";
+        }
     }
 }
--- fizzbuzz-kata/src/test/java/io/franmosteiro/FizzBuzzTestSuite.java
+++ fizzbuzz-kata/src/test/java/io/franmosteiro/FizzBuzzTestSuite.java
@@ -8,6 +8,12 @@ public class FizzBuzzTestSuite {
     @Test
     public void should_print_1_if_1() {
         FizzBuzz classUnderTest = new FizzBuzz();
-        assertEquals("1", classUnderTest.print());
+        assertEquals("1", classUnderTest.print(1));
+    }
+
+    @Test
+    public void should_print_2_if_2() {
+        FizzBuzz classUnderTest = new FizzBuzz();
+        assertEquals("2", classUnderTest.print(2));
     }
 }
