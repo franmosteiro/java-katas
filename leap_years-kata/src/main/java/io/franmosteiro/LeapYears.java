63ba0d8f11152edfeefd9b2ec072375defd9f46a
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-23 12:45:10 +0200

    year_divisible_by_4_and_not_by_100_should_be_leap

--- leap_years-kata/src/main/java/io/franmosteiro/LeapYears.java
+++ leap_years-kata/src/main/java/io/franmosteiro/LeapYears.java
@@ -3,6 +3,11 @@ package io.franmosteiro;
 public class LeapYears {
 
     public boolean isLeap(int year) {
-        return year % 400 == 0;
+        if (year % 400 == 0) {
+            return true;
+        } else if ((year % 4 == 0) && (year % 100 != 0)) {
+            return true;
+        }
+        return false;
     }
 }
--- leap_years-kata/src/test/java/io/franmosteiro/LeapYearsTestSuite.java
+++ leap_years-kata/src/test/java/io/franmosteiro/LeapYearsTestSuite.java
@@ -8,13 +8,24 @@ public class LeapYearsTestSuite {
     @Test
     public void year_divisible_by_400_should_be_leap() {
         LeapYears sut = new LeapYears();
-        assertTrue(sut.isLeap(400));
+        assertTrue(sut.isLeap(2000));
+
     }
 
     @Test
-    public void year_divisible_by_405_should_not_be_leap() {
+    public void year_not_divisible_by_400_should_not_be_leap() {
         LeapYears sut = new LeapYears();
-        assertFalse(sut.isLeap(405));
+        assertFalse(sut.isLeap(2005));
+        assertFalse(sut.isLeap(1700));
+        assertFalse(sut.isLeap(1800));
+        assertFalse(sut.isLeap(1900));
+        assertFalse(sut.isLeap(2100));
+    }
+
+    @Test
+    public void year_divisible_by_4_and_not_by_100_should_be_leap() {
+        LeapYears sut = new LeapYears();
+        assertTrue(sut.isLeap(2008));
     }
 
 }
