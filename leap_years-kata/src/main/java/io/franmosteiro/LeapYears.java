014adb15a64e24517d05f6b13e7ec32904aa0002
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-23 12:35:07 +0200

    400 shoul be leap, baby step

--- leap_years-kata/src/main/java/io/franmosteiro/LeapYears.java
+++ leap_years-kata/src/main/java/io/franmosteiro/LeapYears.java
@@ -2,4 +2,7 @@ package io.franmosteiro;
 
 public class LeapYears {
 
+    public boolean isLeap(int i) {
+        return true;
+    }
 }
--- leap_years-kata/src/test/java/io/franmosteiro/LeapYearsTestSuite.java
+++ leap_years-kata/src/test/java/io/franmosteiro/LeapYearsTestSuite.java
@@ -6,8 +6,9 @@ import static org.junit.Assert.*;
 public class LeapYearsTestSuite {
 
     @Test
-    public void should_be_truthy() {
-        assertTrue(false);
+    public void year_divisible_by_400_should_be_leap() {
+        LeapYears sut = new LeapYears();
+        assertTrue(sut.isLeap(2000));
     }
 
 }
