4d5523933af6fe9ddc5e2001e8ef59f56d1c6c4e
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-23 12:22:12 +0200

    leap years kata skeleton

---
+++ leap_years-kata/src/main/java/io/franmosteiro/LeapYears.java
@@ -0,0 +1,5 @@
+package io.franmosteiro;
+
+public class LeapYears {
+
+}
---
+++ leap_years-kata/src/test/java/io/franmosteiro/LeapYearsTestSuite.java
@@ -0,0 +1,13 @@
+package io.franmosteiro;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LeapYearsTestSuite {
+
+    @Test
+    public void should_be_truthy() {
+        assertTrue(false);
+    }
+
+}
