fb5c0803be0625d64afb529eafc90a040f5d488c
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-23 12:48:27 +0200

    extract rules to methods

--- leap_years-kata/src/main/java/io/franmosteiro/LeapYears.java
+++ leap_years-kata/src/main/java/io/franmosteiro/LeapYears.java
@@ -3,11 +3,19 @@ package io.franmosteiro;
 public class LeapYears {
 
     public boolean isLeap(int year) {
-        if (year % 400 == 0) {
+        if (isDivisibleBy400(year)) {
             return true;
-        } else if ((year % 4 == 0) && (year % 100 != 0)) {
+        } else if (isDivisibleBy4NotBy100(year)) {
             return true;
         }
         return false;
+    }
+
+    private boolean isDivisibleBy400(int year) {
+        return year % 400 == 0;
+    }
+
+    private boolean isDivisibleBy4NotBy100(int year) {
+        return (year % 4 == 0) && (year % 100 != 0);
     }
 }
