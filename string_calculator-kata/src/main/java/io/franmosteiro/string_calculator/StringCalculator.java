1c30f751893c5c07966bf439284d33a1cd4b6d90
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-24 17:06:37 +0200

    refactor prod code. oneliner

--- string_calculator-kata/src/main/java/io/franmosteiro/string_calculator/StringCalculator.java
+++ string_calculator-kata/src/main/java/io/franmosteiro/string_calculator/StringCalculator.java
@@ -5,15 +5,7 @@ import java.math.BigDecimal;
 public class StringCalculator {
 
     public String add(String inputStr) {
-        String overall = "";
         String[] hits = inputStr.split(",");
-        if (hits.length == 1) {
-            if ("".equals(hits[0])) {
-                overall = "0";
-            } else {
-                overall = hits[0];
-            }
-        }
-        return overall;
+        return "".equals(hits[0]) ? "0" : hits[0];
     }
 }
