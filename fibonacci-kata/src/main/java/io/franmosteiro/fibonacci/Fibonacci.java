dd5a725af65d21df1ede995dd487e369d4a0e72a
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-24 09:24:10 +0200

    seq 2 must return 1

--- fibonacci-kata/src/main/java/io/franmosteiro/fibonacci/Fibonacci.java
+++ fibonacci-kata/src/main/java/io/franmosteiro/fibonacci/Fibonacci.java
@@ -2,7 +2,10 @@ package io.franmosteiro.fibonacci;
 
 public class Fibonacci {
 
-    public int calculate(int i) {
-        return i;
+    public int calculate(int sequence) {
+        if (sequence == 2) {
+            return 1;
+        }
+        return sequence;
     }
 }
--- fibonacci-kata/src/test/java/io/franmosteiro/fibonacci/FibonacciTestSuite.java
+++ fibonacci-kata/src/test/java/io/franmosteiro/fibonacci/FibonacciTestSuite.java
@@ -6,15 +6,21 @@ import static org.junit.Assert.*;
 public class FibonacciTestSuite {
 
     @Test
-    public void when_sequence_is_zero_returns_zero() {
+    public void when_sequence_is_0_returns_0() {
         Fibonacci fib = new Fibonacci();
         assertEquals(0, fib.calculate(0));
     }
 
     @Test
-    public void when_sequence_is_one_returns_zero() {
+    public void when_sequence_is_1_returns_0() {
         Fibonacci fib = new Fibonacci();
         assertEquals(1, fib.calculate(1));
+    }
+
+    @Test
+    public void when_sequence_is_2_returns_1() {
+        Fibonacci fib = new Fibonacci();
+        assertEquals(1, fib.calculate(2));
     }
 
 }
