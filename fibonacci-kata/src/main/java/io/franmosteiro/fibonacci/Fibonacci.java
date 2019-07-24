3fd747505eed23a64c1818705a221a53a505f8a8
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-24 09:39:09 +0200

    refactor prod code

--- fibonacci-kata/src/main/java/io/franmosteiro/fibonacci/Fibonacci.java
+++ fibonacci-kata/src/main/java/io/franmosteiro/fibonacci/Fibonacci.java
@@ -4,9 +4,9 @@ public class Fibonacci {
 
     public int calculateNext(int sequence) {
         if (sequence == 2) {
-            return 1;
+            return calculateNext(0) + calculateNext(1);
         } else if (sequence == 3) {
-            return 2;
+            return calculateNext(1) + calculateNext(2);
         } else {
             return sequence;
         }
