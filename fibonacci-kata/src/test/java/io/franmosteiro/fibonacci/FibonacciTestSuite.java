7b3db5bc22e38f588478e8107f28ba7bd28219e0
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-24 11:31:06 +0200

    upgrade junit version to 5.3.1

--- fibonacci-kata/src/test/java/io/franmosteiro/fibonacci/FibonacciTestSuite.java
+++ fibonacci-kata/src/test/java/io/franmosteiro/fibonacci/FibonacciTestSuite.java
@@ -1,7 +1,8 @@
 package io.franmosteiro.fibonacci;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FibonacciTestSuite {
 
