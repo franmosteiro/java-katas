b94edd3fbf866901b5338cde896d4981ff80ccc9
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-24 17:04:14 +0200

    refactor test and prod code, i wasny happy =)

--- string_calculator-kata/src/main/java/io/franmosteiro/string_calculator/StringCalculator.java
+++ string_calculator-kata/src/main/java/io/franmosteiro/string_calculator/StringCalculator.java
@@ -1,11 +1,19 @@
 package io.franmosteiro.string_calculator;
 
+import java.math.BigDecimal;
+
 public class StringCalculator {
 
     public String add(String inputStr) {
-        if (inputStr.length() > 0) {
-            return "1";
+        String overall = "";
+        String[] hits = inputStr.split(",");
+        if (hits.length == 1) {
+            if ("".equals(hits[0])) {
+                overall = "0";
+            } else {
+                overall = hits[0];
+            }
         }
-        return "0";
+        return overall;
     }
 }
--- string_calculator-kata/src/test/java/io/franmosteiro/string_calculator/StringCalculatorTestSuite.java
+++ string_calculator-kata/src/test/java/io/franmosteiro/string_calculator/StringCalculatorTestSuite.java
@@ -1,21 +1,30 @@
 package io.franmosteiro.string_calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTestSuite {
 
+    private StringCalculator strCalc;
+
+    @BeforeEach
+    public void setUp() {
+        strCalc = new StringCalculator();
+    }
+
     @Test
     public void empty_str_should_return_zero() {
-        StringCalculator strCalc = new StringCalculator();
         assertEquals("0", strCalc.add(""));
     }
 
     @Test
-    public void str_should_return_its_values_added() {
-        StringCalculator strCalc = new StringCalculator();
-        assertEquals("1", strCalc.add("0,1"));
+    public void str_containing_one_number_should_return_its_value() {
+        assertEquals("1", strCalc.add("1"));
+        assertEquals("2", strCalc.add("2"));
+        assertEquals("5", strCalc.add("5"));
+        assertEquals("15", strCalc.add("15"));
     }
 
 }
