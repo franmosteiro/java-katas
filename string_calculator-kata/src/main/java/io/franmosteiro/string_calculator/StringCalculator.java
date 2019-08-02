88cf9532d5dfd53793de096a355c30b9fb9a426f
Fran Mosteiro <fmosteiro@theinit.com>
2019-08-02 09:52:31 +0200

    change logic so it can return doubles and not bigdecimals

--- string_calculator-kata/src/main/java/io/franmosteiro/string_calculator/StringCalculator.java
+++ string_calculator-kata/src/main/java/io/franmosteiro/string_calculator/StringCalculator.java
@@ -1,22 +1,20 @@
 package io.franmosteiro.string_calculator;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 public class StringCalculator {
 
     public static final String SEPARATORS_REGEXP = "[,|\\n]";
 
-    public String add(String inputStr) {
+    public Double add(String inputStr) {
 
-        return Arrays.asList(inputStr.split(SEPARATORS_REGEXP)).stream()
-                    .reduce("", (sum1, sum2) -> this.sum(sum1, sum2));
-    }
+        if ("".equals(inputStr)) {
+            return 0d;
+        }
 
-    private String sum(String sum1, String sum2) {
-        return new BigDecimal(sum1.isEmpty() ? "0" : sum1).add(new BigDecimal(sum2.isEmpty() ? "0" : sum2)).toString();
+        return Arrays.asList(inputStr.split(SEPARATORS_REGEXP))
+                    .stream()
+                    .mapToDouble(Double::parseDouble).sum();
     }
     
 }
--- string_calculator-kata/src/test/java/io/franmosteiro/string_calculator/StringCalculatorTestSuite.java
+++ string_calculator-kata/src/test/java/io/franmosteiro/string_calculator/StringCalculatorTestSuite.java
@@ -3,8 +3,6 @@ package io.franmosteiro.string_calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -19,41 +17,43 @@ public class StringCalculatorTestSuite {
 
     @Test
     public void empty_str_should_return_zero() {
-        assertEquals("0", strCalc.add(""));
+        assertEquals(Double.valueOf(0), strCalc.add(""));
     }
 
     @Test
     public void str_containing_one_number_should_return_its_value() {
-        assertEquals("1.1", strCalc.add("1.1"));
-        assertEquals("1", strCalc.add("1"));
-        assertEquals("2", strCalc.add("2"));
-        assertEquals("5", strCalc.add("5"));
-        assertEquals("15", strCalc.add("15"));
+        assertEquals(Double.valueOf(1.1), strCalc.add("1.1"));
+        assertEquals(Double.valueOf(1), strCalc.add("1"));
+        assertEquals(Double.valueOf(2), strCalc.add("2"));
+        assertEquals(Double.valueOf(5), strCalc.add("5"));
+        assertEquals(Double.valueOf(15), strCalc.add("15"));
     }
 
     @Test
     public void str_containing_2_numbers_should_return_their_sum() {
-        assertEquals("3", strCalc.add("1,2"));
-        assertEquals("6", strCalc.add("2,4"));
-        assertEquals("6", strCalc.add("5,1"));
-        assertEquals("16", strCalc.add("15,1"));
-        assertEquals("3.3", strCalc.add("1.1,2.2"));
+        assertEquals(Double.valueOf(3), strCalc.add("1,2"));
+        assertEquals(Double.valueOf(6), strCalc.add("2,4"));
+        assertEquals(Double.valueOf(6), strCalc.add("5,1"));
+        assertEquals(Double.valueOf(16), strCalc.add("15,1"));
     }
 
     @Test
     public void str_containing_3_numbers_should_return_their_sum() {
-        assertEquals("6", strCalc.add("1,2,3"));
-        assertEquals("7", strCalc.add("2,4,1"));
-        assertEquals("6", strCalc.add("5,1,0"));
-        assertEquals("31", strCalc.add("15,1,15"));
-        assertEquals("1.1", strCalc.add("0.4,0.4,0.3"));
+        assertEquals(Double.valueOf(6), strCalc.add("1,2,3"));
+        assertEquals(Double.valueOf(7), strCalc.add("2,4,1"));
+        assertEquals(Double.valueOf(6), strCalc.add("5,1,0"));
+        assertEquals(Double.valueOf(31), strCalc.add("15,1,15"));
+        assertEquals(Double.valueOf(1.1), strCalc.add("0.4,0.4,0.3"));
     }
 
     @Test
     public void str_containing_newLine_asSeparator_should_return_sum_of_containing_numbers() {
-        assertEquals("6", strCalc.add("1\n2,3"));
-        assertEquals("3.3", strCalc.add("1.1\n2.2"));
-        assertEquals("5.3", strCalc.add("1.1,4.2\n"));
+        assertEquals(Double.valueOf(6), strCalc.add("1\n2,3"));
     }
 
+    @Test
+    public void str_containing_newLine_inAnIncorrectPosition_should_fail() {
+        assertThrows(NumberFormatException.class, () -> strCalc.add("175.2,\n35"));
+        assertThrows(NumberFormatException.class, () -> strCalc.add("1.2,3,\n1"));
+    }
 }
