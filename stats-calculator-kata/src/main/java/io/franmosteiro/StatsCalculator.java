f1f3cbb1e76a5fc2ba6657bd1e5c16bccc09fb45
Fran Mosteiro <fmosteiro@theinit.com>
2019-10-24 16:05:03 +0200

    Stats calculator Kata: Solution with streams and static methods

--- stats-calculator-kata/src/main/java/io/franmosteiro/StatsCalculator.java
+++ stats-calculator-kata/src/main/java/io/franmosteiro/StatsCalculator.java
@@ -7,25 +7,24 @@ import java.util.stream.Stream;
 
 public class StatsCalculator {
 
-    private final Stream<Integer> stats;
-
-    public StatsCalculator(Integer[] stats) {
-        this.stats = Arrays.stream(stats);
+    public static int getMinimumValue(Integer[] stats) {
+        return toStream(stats).min(Comparator.naturalOrder()).orElse(0);
     }
 
-    public int getMinimumValue() {
-        return this.stats.min(Comparator.naturalOrder()).orElse(0);
+    public static int getMaximumValue(Integer[] stats) {
+        return toStream(stats).max(Comparator.naturalOrder()).orElse(0);
     }
 
-    public int getMaximumValue() {
-        return this.stats.max(Comparator.naturalOrder()).orElse(0);
+    public static long getNumberOfElements(Integer[] stats) {
+        return toStream(stats).count();
     }
 
-    public long getNumberOfElements() {
-        return this.stats.count();
+    public static double getAverage(Integer[] stats) {
+        return toStream(stats).mapToDouble(a -> a).average().orElse(0.0);
     }
 
-    public double getAverage() {
-        return this.stats.mapToDouble(a -> a).average().orElse(0.0);
+    private static Stream<Integer> toStream(Integer[] stats) {
+        return Arrays.stream(stats);
     }
+    
 }
--- stats-calculator-kata/src/test/java/io/franmosteiro/StatsCalculatorShould.java
+++ stats-calculator-kata/src/test/java/io/franmosteiro/StatsCalculatorShould.java
@@ -1,5 +1,6 @@
 package io.franmosteiro;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,86 +10,70 @@ class StatsCalculatorShould {
     private StatsCalculator calculator;
     private Integer[] stats;
 
+    @BeforeAll
+    void setUp() {
+        this.calculator = new StatsCalculator();
+    }
+
     @Test
     void return_minimun_value_from_input_stats() {
 
         this.stats = new Integer[] { 0, -1 };
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(-1, calculator.getMinimumValue());
+        assertEquals(-1, calculator.getMinimumValue(this.stats));
 
         this.stats = new Integer[] { 12350, 98745 };
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(12350, calculator.getMinimumValue());
+        assertEquals(12350, calculator.getMinimumValue(this.stats));
     }
 
     @Test
     void return_minimum_as_zero_for_empty_input_stats() {
-        this.stats = new Integer[0];
-        this.calculator = new StatsCalculator(this.stats);
 
-        assertEquals(0, calculator.getMinimumValue());
+        this.stats = new Integer[0];
+        assertEquals(0, calculator.getMinimumValue(this.stats));
     }
 
     @Test
     void return_maximum_value_from_input_stats() {
 
         this.stats = new Integer[] { 100, 1324 };
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(1324, calculator.getMaximumValue());
+        assertEquals(1324, calculator.getMaximumValue(this.stats));
 
         this.stats = new Integer[] { -5, 1 };
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(1, calculator.getMaximumValue());
+        assertEquals(1, calculator.getMaximumValue(this.stats));
     }
 
     @Test
     void return_maximum_as_zero_for_empty_input_stats() {
-        this.stats = new Integer[0];
-        this.calculator = new StatsCalculator(this.stats);
 
-        assertEquals(0, calculator.getMaximumValue());
+        this.stats = new Integer[0];
+        assertEquals(0, calculator.getMaximumValue(this.stats));
+
     }
 
     @Test
     void return_number_of_elements_from_input_stats() {
 
         this.stats = new Integer[] { 10, -41, 2, 5 };
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(4, calculator.getNumberOfElements());
+        assertEquals(4, calculator.getNumberOfElements(this.stats));
 
         this.stats = new Integer[] { 10 };
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(1, calculator.getNumberOfElements());
+        assertEquals(1, calculator.getNumberOfElements(this.stats));
 
         this.stats = new Integer[0];
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(0, calculator.getNumberOfElements());
+        assertEquals(0, calculator.getNumberOfElements(this.stats));
     }
 
     @Test
     void return_average_value_from_input_stats() {
 
         this.stats = new Integer[] { 1, -4, 3, 5 };
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(1.25, calculator.getAverage());
+        assertEquals(1.25, calculator.getAverage(this.stats));
 
         this.stats = new Integer[] { 1, 4, 3, 5 };
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(3.25, calculator.getAverage());
+        assertEquals(3.25, calculator.getAverage(this.stats));
 
         this.stats = new Integer[0];
-        this.calculator = new StatsCalculator(this.stats);
-
-        assertEquals(0.0, calculator.getAverage());
+        assertEquals(0.0, calculator.getAverage(this.stats));
     }
 
 }
